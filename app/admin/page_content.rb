0a387e1bdb7a749382289569c08aa9b732873e53
lvl0nax <lvl0nax@gmail.com>
2015-05-27 21:18:28 +0300

    fix cooler tags

--- app/admin/page_content.rb
+++ app/admin/page_content.rb
@@ -1,4 +1,31 @@
 ActiveAdmin.register PageContent do
   permit_params :relation, :main_header, :main_text, :main_image,
   :additional_header, :additional_text, :seo_title, :seo_description, :seo_keywords
+
+  form do |f|
+    f.semantic_errors
+    inputs 'Relation' do
+      input :relation, as: :select, collection: PageContent.relations.keys, label: false
+    end
+    inputs 'Main header' do
+      input :main_header, as: :ckeditor, label: false
+    end
+    inputs 'Main text' do
+      input :main_text, as: :ckeditor, label: false
+    end
+    [:main_image, :additional_header].each do |param|
+      inputs "#{param.capitalize}" do
+        input param, label: false
+      end
+    end
+    inputs 'Additional text' do
+      input :additional_text, as: :ckeditor, label: false
+    end
+    [:seo_title, :seo_description, :seo_keywords].each do |param|
+      inputs "#{param.capitalize}" do
+        input param, label: false
+      end
+    end
+    actions
+  end
 end
--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -1,5 +1,5 @@
 class PageContent < ActiveRecord::Base
   mount_uploader :main_image, ImageUploader
 
-  enum relation: ['service', 'other']
+  enum relation: [:service, :other]
 end
