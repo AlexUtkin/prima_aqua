187118c58584ae5e385fa3860313ad33031a3483
lvl0nax <lvl0nax@gmail.com>
2015-11-24 23:04:25 +0300

    fixes orders for volumes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     @all_delivery_pages ||= ::DeliveryPage.all
     @all_aquas ||= ::Aqua.includes(:volumes).select(:id, :name, :id).order(:id)
     f_aqua = @all_aquas.first
-    @a_volumes = f_aqua.volumes.sort_by(&:title_value)
+    @a_volumes = f_aqua.volumes.sort_by { |v| v.value == 19 && !v.pet && !v.soda ? -20 : -1 * v.value.to_i }
     @aqua_price = ::OrderService.get_price(f_aqua.id, @a_volumes.first.id, 2)
     @deposit = @a_volumes.first.deposit.to_i * 2
     @items = ::Cooler.includes(:images).select(:id, :title, :price).first(10)
--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -2,7 +2,10 @@ class VolumesController < ApplicationController
   skip_before_action :prepare_data, only: :index
   def index
     aqua = Aqua.find(params[:aqua_id])
-    volumes = aqua.volumes.sort_by(&:title_value).map { |v| "#{v.id}-#{v.title_value}" }
+    volumes = aqua
+      .volumes
+      .sort_by { |v| v.value == 19 && !v.pet && !v.soda ? -20 : -1 * v.value.to_i }
+      .map { |v| "#{v.id}-#{v.title_value}" }
     render json: { volumes: volumes }
   end
 end
