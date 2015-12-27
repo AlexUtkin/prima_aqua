ad26a764fb6614ce2da4569d230134bef45cf548
Molchanov Andrey <neodelf@gmail.com>
2015-12-28 00:59:15 +0300

    new products order in products/index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
     type = params[:type] || 'cup'
-    @products = Category.find_by_slug(type).products
+    @products = Category.find_by_slug(type).products.order(:position)
     @categories = Category.all
     render 'products/index'
   end
