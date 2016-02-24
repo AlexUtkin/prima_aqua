ce0da32691f36bccb4b1826d496dc621f3a82fee
Andrey Molchanov <neodelf@gmail.com>
2016-02-25 00:14:06 +0300

    change links to register, work with certificates (cherry picked from commit 1622d6e)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
+url = Rails.env.development? ? 'localhost' : 'redis5.locum.ru'
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis5.locum.ru:6379' }
+  config.redis = { url: "redis://#{url}:6379" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis5.locum.ru:6379' }
+  config.redis = { url: "redis://#{url}:6379" }
 end
